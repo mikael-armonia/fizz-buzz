e7e76b436ff5976d95ab08d0d8fd02626d3cb5b3
Mikaël Armonia <mikael.armonia@gmail.com>
2023-05-12 20:39:07 +0200

    Fix gradle build tools

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.2.0-alpha03")
+    implementation("com.android.tools.build:gradle:8.0.1")
     implementation(kotlin("gradle-plugin", "1.8.10"))
     implementation(kotlin("stdlib", "1.8.10"))
 }
